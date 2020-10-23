7ba5dd2b53934d982d865520dc7dc4d9e1d96b63
Ensar Erdag <ebhan96@hotmail.com>
2020-10-23 15:59:49 +0300

    Initialize MongoDB

---
+++ mongodb.py
@@ -0,0 +1,28 @@
+import os
+from pymongo import MongoClient
+from dotenv import load_dotenv
+
+
+class MongoDB:
+    def __init__(self):
+        load_dotenv(os.path.join(os.getcwd(), 'mongodb-credentials.env'))
+        self.user = os.getenv('user')
+        self.password = os.getenv('password')
+        self.dbname = os.getenv('dbname')
+
+        self.cluster = MongoClient(
+            f"mongodb+srv://botAdmin:{self.password}@whatsappcluster.p1ato.mongodb.net/{self.dbname}?retryWrites=true&w=majority")
+        self.db = self.cluster['WhatsApp']
+        self.collection = self.db['Messages']
+
+    def insert(self, dictionary):
+        return self.collection.insert_one(dictionary)
+
+
+if __name__ == '__main__':
+    mongo = MongoDB()
+
+    post1 = {"name": "lasagna"}
+    post2 = {"_id": 5, "name": "joe"}
+
+    x = mongo.insert(post1)
